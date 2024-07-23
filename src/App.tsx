1ce0fa42850e5f47c3a96a73118d7cf69b4814ba
Richard Sween <sweenr418@gmail.com>
2024-07-23 16:50:08 -0500

    Initial working packing slips

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import logo from './assets/labelfusion.jpeg'
 import './App.css'
-import { PDFDocument } from 'pdf-lib'
+import { PDFDocument, RotationTypes, StandardFonts, rgb } from 'pdf-lib'
+import fontkit from '@pdf-lib/fontkit'
+import pdfToText from 'react-pdftotext'
 
 function App() {
     const pdfInputRef = useRef(null)
+    const [incPackingSlip, setIncPackingSlip] = useState(false)
+    const [incThankYou, setIncThankYou] = useState(false)
 
-    const readFile = (file: File): Promise<PDFDocument> => {
+    const handwritingFontUrl = '/CoveredByYourGrace-Regular.ttf'
+
+    const readFile = (
+        file: File
+    ): Promise<{ pdfDoc: PDFDocument; pdfText: string }> => {
         return new Promise((resolve, reject) => {
             const reader = new FileReader()
 
@@ -17,7 +25,9 @@ function App() {
                     const typedArray = new Uint8Array(arrayBuffer)
                     const pdfDoc = await PDFDocument.load(typedArray)
 
-                    resolve(pdfDoc)
+                    const pdfText = await pdfToText(file)
+
+                    resolve({ pdfDoc, pdfText })
                 }
             }
 
@@ -35,13 +45,103 @@ function App() {
 
             if (files !== null && files?.length > 0) {
                 const combinedPdf = await PDFDocument.create()
+                const timesRomanFont = await combinedPdf.embedFont(
+                    StandardFonts.TimesRoman
+                )
+                const fontBytes = await fetch(handwritingFontUrl).then((res) =>
+                    res.arrayBuffer()
+                )
+                combinedPdf.registerFontkit(fontkit)
+                const customFont = await combinedPdf.embedFont(fontBytes)
 
                 for (let i = 0; i < files.length; i++) {
-                    const pdf = await readFile(files[i])
+                    const { pdfDoc, pdfText } = await readFile(files[i])
 
-                    const [lastPage] = await combinedPdf.copyPages(pdf, [
-                        pdf.getPageCount() - 1,
+                    const [lastPage] = await combinedPdf.copyPages(pdfDoc, [
+                        pdfDoc.getPageCount() - 1,
                     ])
+
+                    if (incPackingSlip) {
+                        const pageDims = lastPage.getSize()
+
+                        const lines = pdfText
+                            .split(/(?<!Livestream )(Name:)|  /g)
+                            .filter((l) => l !== undefined && l !== '')
+                        console.log(lines)
+                        const packingSlip = combinedPdf.addPage([
+                            pageDims.width,
+                            pageDims.height,
+                        ])
+                        const fontSize = 12
+                        let numLines = 0
+                        const itemsStart = lines.indexOf('Name:')
+                        for (let j = 0; j < itemsStart; j++) {
+                            packingSlip.drawText(lines[j], {
+                                x: 10,
+                                y: pageDims.height - numLines * fontSize - 20,
+                                size: fontSize,
+                                lineHeight: fontSize,
+                                font: timesRomanFont,
+                                color: rgb(0, 0, 0),
+                                maxWidth: pageDims.width - 20,
+                                wordBreaks: [' '],
+                            })
+                            numLines =
+                                numLines + Math.ceil(lines[j].length / 50)
+                        }
+                        numLines++
+                        packingSlip.drawText('Items:', {
+                            x: 10,
+                            y: pageDims.height - numLines * fontSize - 20,
+                            size: fontSize,
+                            font: timesRomanFont,
+                            color: rgb(0, 0, 0),
+                        })
+                        numLines++
+                        packingSlip.drawLine({
+                            start: {
+                                x: 10,
+                                y: pageDims.height - numLines * fontSize - 15,
+                            },
+                            end: {
+                                x: pageDims.width - 10,
+                                y: pageDims.height - numLines * fontSize - 15,
+                            },
+                            thickness: 2,
+                            color: rgb(0, 0, 0),
+                            opacity: 0.75,
+                        })
+                        numLines++
+                        for (let j = itemsStart + 1; j < lines.length; j++) {
+                            packingSlip.drawText(lines[j], {
+                                x: 10,
+                                y: pageDims.height - numLines * fontSize - 20,
+                                size: fontSize,
+                                font: timesRomanFont,
+                                color: rgb(0, 0, 0),
+                            })
+                            numLines++
+                            const nextItem = lines.indexOf('Name:', j)
+                            if (nextItem === -1) {
+                                break
+                            }
+                            j = nextItem
+                        }
+
+                        if (incThankYou) {
+                            packingSlip.drawText('Thank you for\nyour order!', {
+                                x: pageDims.width / 2,
+                                y: pageDims.height / 4,
+                                size: fontSize * 2,
+                                font: customFont,
+                                color: rgb(0, 0, 0),
+                                rotate: {
+                                    angle: 45,
+                                    type: RotationTypes.Degrees,
+                                },
+                            })
+                        }
+                    }
 
                     combinedPdf.addPage(lastPage)
                 }
@@ -70,6 +170,26 @@ function App() {
                     ref={pdfInputRef}
                 />
                 <button onClick={generatePdf}>Generate</button>
+            </div>
+            <div className="card">
+                <h2>Options:</h2>
+                <input
+                    type="checkbox"
+                    id="usePackingSlip"
+                    onClick={(e) => setIncPackingSlip(e.target.checked)}
+                />
+                <label htmlFor="usePackingSlip">
+                    Include packing slips [Experimental]
+                </label>
+                <br />
+                <input
+                    type="checkbox"
+                    id="useThankYou"
+                    onClick={(e) => setIncThankYou(e.target.checked)}
+                />
+                <label htmlFor="useThankYou">
+                    Include thank you note on packing slip [Experimental]
+                </label>
             </div>
             <div className="card">
                 <h2>How to use:</h2>
